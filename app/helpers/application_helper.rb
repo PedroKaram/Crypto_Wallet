487b2de2a04244544f0a929e7ffacbf180863254
Karam <pedrokaram4005@gmail.com>
2023-05-29 14:18:41 -0300

    Setting enviroment identifier helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def data_br(data_us)
     data_us.strftime("%d/%m/%Y")
   end
+  
+  def ambiente_rails
+    case Rails.env
+    when "development"
+      "Desenvolvimento"
+    when "production"
+      "Produção"
+    else
+      "Teste"
+    end
+  end
 end
